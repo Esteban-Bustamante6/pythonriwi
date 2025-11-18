35fab0c59e3ad23a4f4718dd58cd31f4e14220e3
Brayhan Barrera <92957789+XxXUtsugiXxX@users.noreply.github.com>
2025-11-18 18:36:18 -0500

    Add files via upload

---
+++ leer.py
@@ -0,0 +1,17 @@
+
+id_estudiante = 1
+nombre = brayhan
+apellido = barrera
+grado = 11
+
+estudientes = {
+    'id' : id_estudiante,
+    'nombre' : nombre,
+    'apellido' : apellido,
+    'grado' : grado
+}
+def leer(estudiantes):
+    for clave, valor in estudiantes.items():
+        print(estudiante)
+        
+leer(estudientes)

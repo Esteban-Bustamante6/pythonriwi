0ca0cdf33915cb05fbb9b404b98c1ab607983807
EstebanBL <estebanbl090@gmail.com>
2025-11-18 20:57:32 -0500

    Update leer.py

--- leer.py
+++ leer.py
@@ -1,17 +1,21 @@
+import json
+
 
 id_estudiante = 1
-nombre = brayhan
-apellido = barrera
+nombre = "brayhan"
+apellido = "barrera"
 grado = 11
 
 estudientes = {
     'id' : id_estudiante,
     'nombre' : nombre,
     'apellido' : apellido,
-    'grado' : grado
+    'grado' : grado,
 }
 def leer(estudiantes):
-    for clave, valor in estudiantes.items():
-        print(estudiante)
+        if not estudiantes:
+            print("no hay información")
+        else:
+            print(f"id: {estudiantes['id']}, nombre: {estudiantes['nombre']}, apellido: {estudiantes['apellido']}, grado: {estudiantes['grado']}")
         
 leer(estudientes)
